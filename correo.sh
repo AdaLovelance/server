1421c50587d3b8aa9bcb48e54fa10018dd517e34
Kao <ada_lovelance@informatico-madrid.com>
2015-05-27 04:10:14 +0200

    Update correo.sh
    resolución de fallo al copiar desde terminal.

--- correo.sh
+++ correo.sh
@@ -31,9 +31,9 @@ echo -e "\nCreando usuaria mailuser...\n"
 mysql -uroot -p$passmariadb << EOF
 USE mailserver;
 GRANT SELECT ON mailserver.* TO 'mailuser'@'127.0.0.1' IDENTIFIED BY '$respuestamailuser'; FLUSH PRIVILEGES;
-CREATE TABLE mailserver.virtual_domains (id int(11) NOT NULL auto_increment,name varchar(50) NOT NULL,PRIMARY KEY (id)) ENGIN$
-CREATE TABLE mailserver.virtual_users (id int(11) NOT NULL auto_increment,domain_id int(11) NOT NULL,password varchar(106) NO$
-CREATE TABLE mailserver.virtual_aliases (id int(11) NOT NULL auto_increment,domain_id int(11) NOT NULL,source varchar(100) NO$
+CREATE TABLE mailserver.virtual_domains (`id` int(11) NOT NULL auto_increment, `name` varchar(50) NOT NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE mailserver.virtual_users (`id` int(11) NOT NULL auto_increment, `domain_id` int(11) NOT NULL,`password` varchar(106) NOT NULL,`email` varchar(100) NOT NULL, PRIMARY KEY (`id`), UNIQUE KEY `email` (`email`),FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE mailserver.virtual_aliases (`id` int(11) NOT NULL auto_increment,`domain_id` int(11) NOT NULL, `source` varchar(100) NOT NULL,`destination` varchar(100) NOT NULL, PRIMARY KEY (`id`), FOREIGN KEY (domain_id) REFERENCES virtual_domains(id) ON DELETE CASCADE) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 EOF
 
 read -p "Introduzca nombre de dominio para su correo [ejemplo.net]: " respuestadominio
